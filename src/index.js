1e2f776e754aa2b0024ea061631a9c1193cba1bf
Bauch <LBauch@tobit.ag>
2020-08-21 17:45:36 +0200

    :construction: working on milestone two

--- src/index.js
+++ src/index.js
@@ -5,12 +5,16 @@ import './app.scss';
 
 chayns.ready.then(() => {
     chayns.ui.accordion.init();
+    showData();
     console.log('Chayns is ready, environment loaded', chayns.env);
 }).catch(() => {
     console.warn('No chayns environment found');
 }).then(() => {
     console.log('Will always be executed');
 });
+
+// const $moreSites = document.querySelector('.moreSites');
+document.querySelector('#moreSites').addEventListener('click', getData);
 
 const handleUserIsLoggedIn = ($introElement, $loginBtn) => {
     $introElement.innerText = `Hallo ${chayns.env.user.firstName}`;
@@ -45,5 +49,19 @@ const init = async () => {
         console.error('No chayns environment found', err);
     }
 };
+const getData = async () => {
+    console.log('ok');
+    const data = await fetch('https://chayns1.tobit.com/TappApi/Site/SlitteApp?SearchString=love&Skip=0&Take=16');
+    const formatedData = await data.json();
+    const arrayData = await formatedData.Data;
+    for (let i = 0; i < arrayData.length; i++) {
+        const locationId = arrayData[i].locationId;
+        console.log(locationId);
+    }
+};
 
+/* const showData = async ($moreSites) => {
+    console.log('running');
+    $moreSites.addEventListener('click', getData);
+}; */
 init();
