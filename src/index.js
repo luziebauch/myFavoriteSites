0dda82ac90c8057e2bbb4804c1aff4498f35c38a
Bauch <LBauch@tobit.ag>
2020-08-26 09:57:12 +0200

    :construction: working on my project with js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 /* eslint-disable no-param-reassign */
 import './app.scss';
 
+let siteCounter = 0;
 chayns.ready.then(() => {
     chayns.ui.accordion.init();
     console.log('Chayns is ready, environment loaded', chayns.env);
@@ -20,8 +21,7 @@ const init = async () => {
     }
 };
 const getData = async () => {
-    console.log('ok');
-    const data = await fetch('https://chayns1.tobit.com/TappApi/Site/SlitteApp?SearchString=love&Skip=0&Take=16');
+    const data = await fetch(`https://chayns1.tobit.com/TappApi/Site/SlitteApp?SearchString=love&Skip=${siteCounter}&Take=24`);
     const formatedData = await data.json();
     const arrayData = await formatedData.Data;
     for (let i = 0; i < arrayData.length; i++) {
@@ -33,19 +33,19 @@ const getData = async () => {
         const container = document.createElement('div');
         container.classList.add('element');
         const background = document.createElement('div');
-        const name = document.createElement('p');
+        const name = document.createElement('div');
         const image = document.createElement('div');
         image.classList.add('image');
         background.classList.add('image');
-
-        name.innerHTML = appstoreName;
-        //background.addEventListener('click', `https://sub60.tobit.com/l/${siteId}`);  
-        background.style = `background-image: url(https://sub60.tobit.com/l/152342?size=72)`;
-        image.style = `background-image: url(https://sub60.tobit.com/l/${locationId}?size=72)`;
+        //background.addEventListener('click', `https://sub60.tobit.com/l/${siteId}`);
+        name.innerHTML = appstoreName.substring(0, 11);
+        background.style = `background-image: url(https://sub60.tobit.com/l/152342?size=70)`;
+        image.style = `background-image: url(https://sub60.tobit.com/l/${locationId}?size=70)`;
         $list.appendChild(container);
         container.appendChild(background);
         background.appendChild(image);
         container.appendChild(name);
     }
+    siteCounter += 24;
 };
 init();
